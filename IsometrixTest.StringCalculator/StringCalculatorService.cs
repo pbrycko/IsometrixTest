e7a49a0dd97378869c377695b8071bc4cfbc9415
TehGM <TehGM@users.noreply.github.com>
2022-01-31 14:05:38 +0100

    Pass first 2 steps of first task

--- IsometrixTest.StringCalculator/StringCalculatorService.cs
+++ IsometrixTest.StringCalculator/StringCalculatorService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IsometrixTest.StringCalculator
 {
@@ -6,7 +8,16 @@ namespace IsometrixTest.StringCalculator
     {
         public int Add(string expression)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(expression))
+                return 0;
+            IEnumerable<int> numbers = this.GetNumbers(expression);
+            return numbers.Sum();
+        }
+
+        private IEnumerable<int> GetNumbers(string expression)
+        {
+            string[] numbers = expression.Split(',');
+            return numbers.Select(num => Convert.ToInt32(num));
         }
     }
 }
